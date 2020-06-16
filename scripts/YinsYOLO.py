81db3ccaed2670690b86c6c0edd2931aae7067c0
Yiqiao <eagle0504@gmail.com>
2020-06-16 18:06:01 -0400

    stealthmode added aka invisibility cloak

--- scripts/YinsYOLO.py
+++ scripts/YinsYOLO.py
@@ -758,3 +758,73 @@ class YinsYOLO:
         # release resources
         webcam.release()
         cv2.destroyAllWindows()
+        
+    # Define function
+    def StealthMode():
+        # library
+        import cv2
+        import numpy as np
+        import time
+
+        def recordBackground():
+            print("""
+                      Recording Background .....................
+                """)
+            cap = cv2.VideoCapture(0)
+            time.sleep(1)
+            background=0
+            for i in range(30):
+                ret,background = cap.read()
+            cap.release()
+            cv2.destroyAllWindows()
+            background = np.flip(background,axis=1)
+            print("""
+                     Background recorded !!
+                """)
+            return background
+
+        def getInvisible():
+            background=recordBackground()
+
+            print("""
+                     Get ready to become invisible .....................
+                """)
+            cap = cv2.VideoCapture(0)
+            while(cap.isOpened()):
+                ret, img = cap.read()
+                img = np.flip(img,axis=1)
+
+                # Converting image to HSV color space.
+                hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                value = (35, 35)
+
+                blurred = cv2.GaussianBlur(hsv, value,0)
+
+                lower_red = np.array([0,120,70])
+                upper_red = np.array([10,255,255])
+                mask1 = cv2.inRange(hsv,lower_red,upper_red)
+
+                lower_red = np.array([170,120,70])
+                upper_red = np.array([180,255,255])
+                mask2 = cv2.inRange(hsv,lower_red,upper_red)
+
+                # Addition of the two masks to generate the final mask.
+                mask = mask1+mask2
+                mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5),np.uint8))
+
+                # Replacing pixels corresponding to cloak with the background pixels.
+                img[np.where(mask==255)] = background[np.where(mask==255)]
+                cv2.imshow('Display',img)
+                k = cv2.waitKey(1)
+                if k == 27:
+                    cap.release()
+                    break
+
+                # press "Q" to stop
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            cv2.destroyAllWindows()
+
+        # Run function
+        getInvisible()
